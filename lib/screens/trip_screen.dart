5e4731b872578ee8b0def5493638ee671a9406a0
Tomas Sykora, jr <jump@syky.cz>
2020-05-12 00:43:46 +0200

    WIP: Trip Form

--- lib/screens/trip_screen.dart
+++ lib/screens/trip_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sss_mobile/models/trip.dart';
 
 class TripScreen extends StatefulWidget {
   final Trip trip;
 
-  TripScreen({Key key, this.trip}) : super(key: key);
+  TripScreen({Key key, Trip trip})
+      : trip = trip ?? Trip(); // : super(key: key);
 
   @override
   _TripScreenState createState() => _TripScreenState(trip);
@@ -14,20 +16,133 @@ class TripScreen extends StatefulWidget {
 class _TripScreenState extends State<TripScreen> {
   final _formKey = GlobalKey();
 
-  final Trip trip;
+  Trip trip = Trip();
 
   _TripScreenState(this.trip);
+
+  var beginDateController = TextEditingController();
+  var endDateController = TextEditingController();
+
+  Future<Null> _selectDate(BuildContext context, bool begin) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+
+    if (picked != null)
+      setState(() {
+        if (begin) {
+          trip.beginDate = picked;
+          beginDateController.text = trip.beginDate.toString();
+        } else {
+          trip.endDate = picked;
+          endDateController.text = trip.beginDate.toString();
+        }
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: trip != null ? Text('Upravit jízdu') : Text('Nová jízda')),
+            title:
+            trip.id != null ? Text('Upravit jízdu') : Text('Nová jízda')),
         body: Container(
             child: Builder(
                 builder: (context) => Form(
-                      key: _formKey,
-                      child: Container(),
-                    ))));
+                    key: _formKey,
+                    child: Container(
+                      child: Column(children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: TextFormField(
+                                  keyboardType: TextInputType.numberWithOptions(
+                                      decimal: true, signed: false),
+                                  decoration: InputDecoration(
+                                      labelText: 'Počáteční stav km'),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'Zadejte prosím hodnotu';
+                                    }
+                                    return null;
+                                  },
+                                  onSaved: (val) =>
+                                      setState(() =>
+                                      trip.beginOdometer = int.parse(val)),
+                                ),
+                              ),
+                            ),
+                            Flexible(
+                              child: Container(
+                                padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: TextFormField(
+                                  keyboardType: TextInputType.numberWithOptions(
+                                      decimal: true, signed: false),
+                                  decoration: InputDecoration(
+                                      labelText: 'Konečný stav km'),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return 'Zadejte prosím hodnotu';
+                                    }
+                                    return null;
+                                  },
+                                  onSaved: (val) =>
+                                      setState(
+                                              () =>
+                                          trip.endOdometer = int.parse(val)),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Flexible(
+                              child: Container(
+                                padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                      labelText: 'Počáteční datum'),
+                                  controller: beginDateController,
+                                  initialValue: trip.beginDate != null
+                                      ? trip.beginDate.toString()
+                                      : "",
+                                  onTap: () {
+                                    _selectDate(context, true);
+                                  },
+                                  readOnly: true,
+                                ),
+                              ),
+                            ),
+                            Flexible(
+                              child: Container(
+                                padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                      labelText: 'Konečné datum'),
+                                  controller: endDateController,
+                                  initialValue: trip.endDate != null
+                                      ? trip.endDate.toString()
+                                      : "",
+                                  onTap: () {
+                                    _selectDate(context, true);
+                                  },
+                                  readOnly: true,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ]),
+                    )))));
   }
 }
